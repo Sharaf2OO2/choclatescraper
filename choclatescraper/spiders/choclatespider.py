c98bc4878920c0568746a93c83ac8d83b5470c6f
Sharaf Ahmed <sharafahmed2002@gmail.com>
2024-04-05 20:51:39 +0200

    Scraping one page (Name, Price, URL)

--- choclatescraper/spiders/choclatespider.py
+++ choclatescraper/spiders/choclatespider.py
@@ -4,7 +4,14 @@ import scrapy
 class ChoclatespiderSpider(scrapy.Spider):
     name = "choclatespider"
     allowed_domains = ["chocolate.co.uk"]
-    start_urls = ["https://chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"]
 
     def parse(self, response):
-        pass
+        
+        products = response.css('product-item')
+        for product in products:
+            yield{
+                'Name': product.css('a.product-item-meta__title::text').get(),
+                'Price': product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
+                'URL':  product.css('a.product-item-meta__title').attrib['href']
+            }
